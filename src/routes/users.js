4d3f43b07982f38eababbd14aa117b2817844633
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2020-01-08 10:48:09 -0500

    first commit

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-export default class Users extends React.Component{
+class Users extends React.Component{
   constructor(props){
     super(props);
     this.arr = [
       {id:1, content:"Gayathri"},
       {id:2, content:"Nannapaneni"},
       {id:3, content:"gayathri123@gmail.com"}
-    ]
+    ] 
   }
+
+  calledFromChild = (data) => {
+    console.log("data received from child component" + data)
+  }
+
   render() {
     return(
       <ul>
@@ -20,15 +25,20 @@ export default class Users extends React.Component{
 }
 
 class UsersList extends React.Component{    
-  handleClick = () => {
+handleClick = () => {
     this.props.onClick(this.props.data.id);
-  }
+this.props.callParentMethod(this.props.data.id) }
+
+  
     render() {  
+      <UsersList data={element} onClick={this.props.onClick} callParentMethod={this.calledFromChild} />
         return( 
             <li 
                 onClick={this.handleClick}>
                 {this.props.data.content}
             </li>
         )
-    }
+    } 
 }
+
+export default UsersList;
